c0ac03c847e699676abebd2b98db8f923989b97f
rodskades <rodolpho_kades@hotmail.com>
2022-09-20 20:10:10 -0300

    Function to organize the subtitles

---
+++ utils/legendas.py
@@ -0,0 +1,14 @@
+# This Python file uses the following encoding: utf-8
+
+
+def subtitle(label, text, h=700):
+    label.config(text=text)
+    label.master.overrideredirect(True)
+    label.master.geometry(f"+400+{h}")
+    label.master.lift()
+    label.master.wm_attributes("-topmost", True)
+    label.master.wm_attributes("-disabled", True)
+    label.master.wm_attributes("-transparentcolor", "black")
+
+# O programa principal deve ter o label definido da seguinte forma:
+# label = tk.Label(text="", font=('Consolas', '30'), fg='DarkOrange2', bg='black')
